42d7ca070b2ea0bb23ecd174df191b4d68c9bdac
Kevin Harrington <mad.hephaestus@gmail.com>
2023-10-10 14:55:54 -0400

    hi this is ella

--- SOSArdino.ino
+++ SOSArdino.ino
@@ -8,45 +8,29 @@ void setup() {
 	myServo.attach(11); 
 	myServo.write(90);
 }
-
-
 void blinkOnce(int time){ 
   digitalWrite(LED_BUILTIN, HIGH);
-  myServo.write(90);
+  myServo.write(70);
   delay(time);
   digitalWrite(LED_BUILTIN, LOW);
-  myServo.write(70);
+  myServo.write(90);
   delay(time);
 }
 
 void loop() {
-  blinkOnce(1000); 
+  blinkOnce(1000);  
 }
 
+void runSOS(){
+blinkOnce(700);
+blinkOnce(700);
+blinkOnce(700);
 
+blinkOnce(300);
+blinkOnce(300);
+blinkOnce(300);
 
-
-#include <Servo.h> 
-
-Servo myServo;
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-	myServo.attach(11); 
-	myServo.write(90);
-}
-
-
-void blinkOnce(int time){ 
-  digitalWrite(LED_BUILTIN, HIGH);
-  myServo.write(90);
-  delay(time);
-  digitalWrite(LED_BUILTIN, LOW);
-  myServo.write(70);
-  delay(time);
-}
-
-void loop() {
-  blinkOnce(1000); 
+blinkOnce(700);
+blinkOnce(700);
+blinkOnce(700);
 }
