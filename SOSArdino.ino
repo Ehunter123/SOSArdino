0caf701a60fd9317f538d74112e8d76e49a174a1
Kevin Harrington <mad.hephaestus@gmail.com>
2023-10-10 14:31:07 -0400

    Because

--- SOSArdino.ino
+++ SOSArdino.ino
@@ -12,8 +12,38 @@ void setup() {
 
 void blinkOnce(int time){ 
   digitalWrite(LED_BUILTIN, HIGH);
+  myServo.write(90);
   delay(time);
   digitalWrite(LED_BUILTIN, LOW);
+  myServo.write(70);
+  delay(time);
+}
+
+void loop() {
+  blinkOnce(1000); 
+}
+
+
+
+
+#include <Servo.h> 
+
+Servo myServo;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+	myServo.attach(11); 
+	myServo.write(90);
+}
+
+
+void blinkOnce(int time){ 
+  digitalWrite(LED_BUILTIN, HIGH);
+  myServo.write(90);
+  delay(time);
+  digitalWrite(LED_BUILTIN, LOW);
+  myServo.write(70);
   delay(time);
 }
 
